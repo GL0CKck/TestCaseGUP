17153b84620cc3c4a9ec3432b2c6af9e04ba82cb
GL0CKck <78853462+GL0CKck@users.noreply.github.com>
2022-01-04 22:35:49 +0200

    depa

--- testcasegup/settings.py
+++ testcasegup/settings.py
@@ -143,4 +143,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 db_from_env = dj_database_url.config()
-DATABASE['default'].update(db_from_env)
+
